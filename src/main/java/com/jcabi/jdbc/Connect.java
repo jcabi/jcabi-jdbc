7e0202932c473a9ea2c4a06e5d72d16ebf7f6ce0
andreoss <andreoss@sdf.org>
2020-07-09 19:39:02 -0400

    (#81) Fix indentation

--- src/main/java/com/jcabi/jdbc/Connect.java
+++ src/main/java/com/jcabi/jdbc/Connect.java
@@ -43,6 +43,7 @@ interface Connect {
 
     /**
      * Create prepare statement.
+     *
      * @param conn Open connection
      * @return The statement
      * @throws SQLException If some problem
@@ -52,66 +53,78 @@ interface Connect {
     /**
      * Connect which opens a <b>CallableStatement</b>, which
      * is used for calling stored procedures.
+     *
+     * @since 0.13
      */
     final class Call implements Connect {
         /**
          * SQL function call.
          */
         private final String sql;
+
         /**
          * Ctor.
+         *
          * @param query Query
          */
         Call(final String query) {
             this.sql = query;
         }
+
         @Override
-        public PreparedStatement open(final Connection conn)
-            throws SQLException {
+        public PreparedStatement open(final Connection conn) throws SQLException {
             return conn.prepareCall(this.sql);
         }
     }
 
     /**
      * Plain, without keys.
+     *
+     * @since 0.13
      */
     final class Plain implements Connect {
         /**
          * SQL query.
          */
         private final transient String sql;
+
         /**
          * Ctor.
+         *
          * @param query Query
          */
         Plain(final String query) {
             this.sql = query;
         }
+
         @Override
-        public PreparedStatement open(final Connection conn)
-            throws SQLException {
+        public PreparedStatement open(final Connection conn) throws SQLException {
             return conn.prepareStatement(this.sql);
         }
     }
 
     /**
      * With returned keys.
+     *
+     * @since 0.13
      */
     final class WithKeys implements Connect {
         /**
          * SQL query.
          */
         private final transient String sql;
+
         /**
          * Ctor.
+         *
          * @param query Query
          */
         WithKeys(final String query) {
             this.sql = query;
         }
+
         @Override
-        public PreparedStatement open(final Connection conn)
-            throws SQLException {
+        public PreparedStatement open(final Connection conn) throws SQLException {
             return conn.prepareStatement(
                 this.sql,
                 Statement.RETURN_GENERATED_KEYS
