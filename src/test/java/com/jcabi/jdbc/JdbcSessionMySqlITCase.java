0de018eddaa0238228d556b98e764735d317693f
andreoss <andreoss@sdf.org>
2020-12-13 07:17:03 +0300

    (#49) Add more tests

--- src/test/java/com/jcabi/jdbc/JdbcSessionMySqlITCase.java
+++ src/test/java/com/jcabi/jdbc/JdbcSessionMySqlITCase.java
@@ -30,7 +30,12 @@
 package com.jcabi.jdbc;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.CallableStatement;
+import java.sql.Types;
 import javax.sql.DataSource;
+import org.apache.commons.lang3.StringUtils;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.MySQLContainer;
@@ -44,6 +49,7 @@ import org.testcontainers.utility.DockerImageName;
  * @since 0.17.6
  */
 @Testcontainers(disabledWithoutDocker = true)
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
 final class JdbcSessionMySqlITCase {
 
     /**
@@ -57,22 +63,101 @@ final class JdbcSessionMySqlITCase {
                 .asCompatibleSubstituteFor("mysql")
         );
 
-    /**
-     * JdbcSession can do PostgreSQL manipulations.
-     *
-     * @throws Exception If there is some problem inside
-     */
     @Test
     void worksWithExecute() throws Exception {
         final DataSource source = this.source();
         new JdbcSession(source)
             .autocommit(false)
-            .sql("CREATE TABLE IF NOT EXISTS foo (name VARCHAR(50))")
+            .sql("CREATE TABLE foo (name VARCHAR(50))")
             .execute()
             .sql("INSERT INTO foo (name) VALUES (?)")
             .set("Jeff Lebowski")
             .execute()
             .commit();
+    }
+
+    @Test
+    void worksLastInsertId() throws Exception {
+        new JdbcSession(this.source())
+            .sql(
+                StringUtils.join(
+                    "CREATE TABLE IF NOT EXISTS foo (",
+                    "id INT NOT NULL AUTO_INCREMENT, ",
+                    "name VARCHAR(50), ",
+                    "PRIMARY KEY (id)",
+                    ")"
+                )
+            )
+            .execute();
+        MatcherAssert.assertThat(
+            new JdbcSession(this.source())
+                .sql("INSERT INTO foo (name) VALUES (?)")
+                .set("test")
+                .insert(Outcome.LAST_INSERT_ID),
+            Matchers.is(1L)
+        );
+    }
+
+    @Test
+    void worksLastInsertIdAndTransaction() throws Exception {
+        new JdbcSession(this.source())
+            .sql(
+                StringUtils.join(
+                    "CREATE TABLE IF NOT EXISTS foo (",
+                    "id INT NOT NULL AUTO_INCREMENT, ",
+                    "name VARCHAR(50), ",
+                    "PRIMARY KEY (id)",
+                    ")"
+                )
+            )
+            .execute();
+        final JdbcSession session = new JdbcSession(this.source());
+        MatcherAssert.assertThat(
+            session
+                .autocommit(false)
+                .sql("START TRANSACTION")
+                .execute()
+                .sql("INSERT INTO foo (name) VALUES (?)")
+                .set("test")
+                .insert(Outcome.LAST_INSERT_ID),
+            Matchers.is(1L)
+        );
+        session.rollback();
+    }
+
+    @Test
+    void callsFunctionWithOutParam() throws Exception {
+        new JdbcSession(this.source())
+            .autocommit(false)
+            .sql("CREATE TABLE IF NOT EXISTS users (name VARCHAR(50))")
+            .execute()
+            .sql("INSERT INTO users (name) VALUES (?)")
+            .set("Jeff Charles").execute()
+            .sql(
+                StringUtils.join(
+                    "CREATE PROCEDURE proc(OUT username text, OUT day date) ",
+                    "BEGIN SELECT name, CURDATE() INTO username, day ",
+                    "FROM users; ",
+                    "SELECT username, day; ",
+                    "END"
+                )
+            ).execute().commit();
+        final Object[] result = new JdbcSession(this.source())
+            .sql("CALL proc(?, ?)")
+            .prepare(
+                stmt -> {
+                    ((CallableStatement) stmt).registerOutParameter(1, Types.VARCHAR);
+                    ((CallableStatement) stmt).registerOutParameter(2, Types.DATE);
+                }
+            )
+            .call(new StoredProcedureOutcome<>(1, 2));
+        MatcherAssert.assertThat(
+            result,
+            Matchers.arrayContaining(
+                Matchers.is("Jeff Charles"),
+                Matchers.notNullValue()
+            )
+        );
     }
 
     /**
@@ -87,4 +172,5 @@ final class JdbcSessionMySqlITCase {
         src.setPassword(this.container.getPassword());
         return src;
     }
+
 }
