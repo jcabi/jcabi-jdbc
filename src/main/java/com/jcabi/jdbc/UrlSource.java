8fe72a887524698fe62b37b862021cbd8b546319
Yegor Bugayenko <yegor256@gmail.com>
2023-06-12 10:35:44 +0300

    #224 UrlSource

---
+++ src/main/java/com/jcabi/jdbc/UrlSource.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2012-2023, jcabi.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the jcabi.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jcabi.jdbc;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * Data source when all you have is a URL.
+ *
+ * @since 0.19.0
+ */
+@ToString
+@EqualsAndHashCode(of = "url")
+public final class UrlSource implements DataSource {
+
+    /**
+     * The URL.
+     */
+    private final transient String url;
+
+    /**
+     * Public ctor.
+     * @param jdbc The JDBC URL.
+     */
+    public UrlSource(final String jdbc) {
+        this.url = jdbc;
+    }
+
+    @Override
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(this.url);
+        } catch (final SQLException ex) {
+            throw new IllegalArgumentException(ex);
+        }
+    }
+
+    @Override
+    public Connection getConnection(final String username,
+        final String password) {
+        return this.getConnection();
+    }
+
+    @Override
+    public PrintWriter getLogWriter() {
+        throw new UnsupportedOperationException("#getLogWriter()");
+    }
+
+    @Override
+    public void setLogWriter(final PrintWriter writer) {
+        throw new UnsupportedOperationException("#setLogWriter()");
+    }
+
+    @Override
+    public void setLoginTimeout(final int seconds) {
+        throw new UnsupportedOperationException("#setLoginTimeout()");
+    }
+
+    @Override
+    public int getLoginTimeout() {
+        throw new UnsupportedOperationException("#getLoginTimeout()");
+    }
+
+    @Override
+    public Logger getParentLogger() {
+        throw new UnsupportedOperationException("#getParentLogger()");
+    }
+
+    @Override
+    public <T> T unwrap(final Class<T> iface) {
+        throw new UnsupportedOperationException("#unwrap()");
+    }
+
+    @Override
+    public boolean isWrapperFor(final Class<?> iface) {
+        throw new UnsupportedOperationException("#isWrapperFor()");
+    }
+
+}
